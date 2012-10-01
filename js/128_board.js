77174e7a7a0fff189bc797ab902fd6c8bb3df988
Edoardo Batini <eodbat@gmail.com>
2012-10-02 01:49:03 +0200

    JsDoc documentation strings.

--- js/128_board.js
+++ js/128_board.js
@@ -1,8 +1,26 @@
 var debugging_object;
 var board = new Object();
+
+
+/**
+ * Stores the actual position on the board.
+ *
+ * Attributes:
+ *   1) pieces
+ *   2) trait
+ *   3) castling capabilities
+ *   4) enpassant
+ *   5) halfmove
+ *   6) fullmove
+ *
+ * @type {Object}
+ */
 board.position = new Object();
 
-/** Creates an array of 128 zeroes.
+
+/**
+ * Creates an array of 128 zeroes.
+ *
  * @return {Array.<number>} An array of 128 zeroes.
  */
 board.initZerosArray = function() {
@@ -13,22 +31,28 @@ board.initZerosArray = function() {
     return result;
 };
 
+
 /**
  * String representation of all columns.
+ *
  * @type {string}
  * @const
  */
 board.COLUMNS = 'abcdefgh';
 
+
 /**
  * Defines a binary color table for squares.
+ *
  * @type {Object.<number, string>}
  * @const
  */
 board.COLOR = { 0: 'white', 1: 'black' };
 
+
 /**
  * Defines the color of each board square.
+ *
  * @type {Array.<number>}
  * @const
  */
@@ -42,12 +66,21 @@ board.COLORS = [
     0, 1, 0, 1, 0, 1, 0, 1, -1, -1, -1, -1, -1, -1, -1, -1,
     1, 0, 1, 0, 1, 0, 1, 0, -1, -1, -1, -1, -1, -1, -1, -1];
 
+
 /**
  * Represents pieces positions on the board.
+ *
  * @type {Array.<(number|string)>}
  */
 board.position.pieces = board.initZerosArray();
 
+
+/**
+ * Table of pieces' algebraic notation names and full pieces' names.
+ *
+ * @type {Object.<string, string>}
+ * @const
+ */
 board.piecesAbbreviation = {
     'P': 'pawn',
     'p': 'pawn',
@@ -64,6 +97,12 @@ board.piecesAbbreviation = {
 };
 
 
+/**
+ * After being initialized with generateValidMovesTable it contains all the
+ * valid squares for a given piece starting from a given square.
+ *
+ * @type {Object.<string, Array.<number>>
+ */
 board.validMovesTable = {
     'pawn': new Object(),
     'knight': new Object(),
@@ -84,6 +123,7 @@ board.resetPieces = function() {
 /**
  * Checks wether a square is on the left side of the 0x88 board (the valid
  * side).
+ *
  * @param {number} square The square number.
  * @return {boolean} True if square is on board, false otherwise.
  */
@@ -91,26 +131,61 @@ board.onBoard = function(square) {
     return (square & 0x88) == 0;
 };
 
+/**
+ * Get the column number of a square starting from 0.
+ *
+ * @param {number} square The square.
+ * @return {number} The square's column.
+ */
 board.getColumn = function(square) {
     return square & 0x7;
 };
 
+
+/**
+ * Get the rank number of a square starting from 0.
+ *
+ * @param {number} square The square.
+ * @return {number} The square's rank.
+ */
 board.getRank = function(square) {
     return square >> 4;
 };
 
+
+/**
+ * Translates a square number into algebraic notation.
+ *
+ * @param {number} square The square.
+ * @return {string} The square in algebraic notation.
+ */
 board.numberToAlgebraic = function(square) {
     var column = square & 0x7;
     var rank = board.getRank(square);
     return board.COLUMNS[column] + (rank + 1);
 };
 
+
+/**
+ * Translates the algebraic notation string of a square into its corresponding
+ * board number.
+ *
+ * @param {string} square The algebraic notation string.
+ * @return {number} The square index in the board array.
+ */
 board.algebraicToNumber = function(square) {
     var column = board.COLUMNS.indexOf(square[0]);
     var rank = square[1] - 1;
     return (0x10 * rank) + column;
 };
 
+
+/**
+ * Get the color of a piece.
+ *
+ * @param {string} piece_abbr The piece name in algebraic notation.
+ * @return {'w'|'b'} White or Black.
+ */
 board.getPieceColor = function(piece_abbr) {
     if (piece_abbr == piece_abbr.toUpperCase()) {
         return 'w';
@@ -119,6 +194,10 @@ board.getPieceColor = function(piece_abbr) {
     }
 };
 
+
+/**
+ * Toggles board.position.trait from 'w' to 'b' and vice versa.
+ */
 board.position.toggleTrait = function() {
     if (board.position.trait == 'w') {
         board.position.trait = 'b';
@@ -127,9 +206,16 @@ board.position.toggleTrait = function() {
     }
 };
 
+
+/**
+ * Reset to zero the given square.
+ *
+ * @param {number} square The square.
+ */
 board.removeCaptured = function(square) {
     board.position.pieces[square] = 0;
 };
+
 
 /**
  * Place the pieces on the board according to a FEN string if given otherwise
@@ -177,6 +263,38 @@ board.setupFromFen = function(fen_string) {
     }
 };
 
+
+/**
+ * Initialize the validMovesTable with all valid moves of minor and major
+ * pieces (pawns excluded).
+ */
+board.generateValidMovesTable = function() {
+    var piece_function = {
+        'knight': board.validKnightMoves,
+        'bishop': board.validBishopMoves,
+        'rook': board.validRookMoves,
+        'queen': board.validQueenMoves,
+        'king': board.validKingMoves
+    };
+    for (var index = 0; index < 0x80; index++) {
+        if (board.onBoard(index)) {
+            for (var piece in piece_function) {
+                board.validMovesTable[piece][index] =
+                    piece_function[piece](index);
+            }
+        }
+    }
+};
+
+
+/**
+ * Calculate the valid destination squares of a sliding (Bishops, Rooks, Queen)
+ * piece starting from a given square and given its move deltas.
+ *
+ * @param {number} start The starting square.
+ * @param {Array.<number>} move_deltas All possible move's directions.
+ * @return {Array.<number>} The array of all valid destination squares.
+ */
 board.validSlidingMoves = function(start, move_deltas) {
     var moves = [];
     for (var index = 0; index < move_deltas.length; index++) {
@@ -190,6 +308,15 @@ board.validSlidingMoves = function(start, move_deltas) {
     return moves;
 };
 
+
+/**
+ * Calculate the valid destination squares of a not-sliding (King, Knights)
+ * piece starting from a given square and given its move deltas.
+ *
+ * @param {number} start The starting square.
+ * @param {Array.<number>} move_deltas All possible move's directions.
+ * @return {Array.<number>} The array of all valid destination squares.
+ */
 board.validNotSlidingMoves = function(start, move_deltas) {
     var moves = [];
     for (var index = 0; index < move_deltas.length; index++) {
@@ -202,25 +329,66 @@ board.validNotSlidingMoves = function(start, move_deltas) {
     return moves;
 };
 
+
+/**
+ * Calculate the valid destination squares of a Bishop starting from a given
+ * square.
+ *
+ * @param {number} start The starting square.
+ * @return {Array.<number>} The array of all valid destination squares.
+ */
 board.validBishopMoves = function(start) {
     var move_deltas = [17, 15, -17, -15];
     return board.validSlidingMoves(start, move_deltas);
 };
 
+
+/**
+ * Calculate the valid destination squares of a Rook starting from a given
+ * square.
+ *
+ * @param {number} start The starting square.
+ * @return {Array.<number>} The array of all valid destination squares.
+ */
 board.validRookMoves = function(start) {
     var move_deltas = [16, 1, -16, -1];
     return board.validSlidingMoves(start, move_deltas);
 };
 
+
+/**
+ * Calculate the valid destination squares of a Queen starting from a given
+ * square.
+ *
+ * It just sums up Rook moves and Bishop moves.
+ *
+ * @param {number} start The starting square.
+ * @return {Array.<number>} The array of all valid destination squares.
+ */
 board.validQueenMoves = function(start) {
     return board.validBishopMoves(start).concat(board.validRookMoves(start));
 };
 
+
+/**
+ * Calculate the valid destination squares of a Knight starting from a given
+ * square.
+ *
+ * @param {number} start The starting square.
+ * @return {Array.<number>} The array of all valid destination squares.
+ */
 board.validKnightMoves = function(start) {
     var move_deltas = [14, 18, 31, 33, -14 , -18, -31, -33];
     return board.validNotSlidingMoves(start, move_deltas);
 };
 
+/**
+ * Calculate the valid destination squares of a King starting from a given
+ * square.
+ *
+ * @param {number} start The starting square.
+ * @return {Array.<number>} The array of all valid destination squares.
+ */
 board.validKingMoves = function(start) {
     var move_deltas = [16, 17, 1, -15, -16, -17, -1, 15];
     return board.validNotSlidingMoves(start, move_deltas);
@@ -246,57 +414,39 @@ board.validKingMoves = function(start) {
  * @param {number} start The starting square.
  */
 board.validPawnMoves = function(color, start) {
-        var rank_delta = 0x10;
-        var step, diagonals = [];
-        var moves = [];
-        if (color == 'w') {
-            diagonals = [15, 17];
-            if (board.position.pieces[start + rank_delta] == 0) {
-                step = [start + rank_delta];
-            }
-            if (board.getRank(start) == 1) {
-                step = step.concat([start + (rank_delta * 2)]);
-            }
-        } else {
-            diagonals = [-15, -17];
-            if (board.position.pieces[start - rank_delta] == 0) {
-                step = [start - rank_delta];
-            }
-            if (board.getRank(start) == 6) {
-                step = step.concat([start - (rank_delta * 2)]);
-            }
+    var rank_delta = 0x10;
+    var step, diagonals = [];
+    var moves = [];
+    if (color == 'w') {
+        diagonals = [15, 17];
+        if (board.position.pieces[start + rank_delta] == 0) {
+            step = [start + rank_delta];
         }
-        for (var delta in diagonals) {
-            if (board.position.pieces[start + diagonals[delta]] != 0) {
-                moves.push(start + diagonals[delta]);
-            }
+        if (board.getRank(start) == 1) {
+            step = step.concat([start + (rank_delta * 2)]);
         }
-        moves = moves.concat(step);
-        board.validMovesTable['pawn'][start] = moves;
-};
-
-board.generateValidMovesTable = function() {
-    var piece_function = {
-        'knight': board.validKnightMoves,
-        'bishop': board.validBishopMoves,
-        'rook': board.validRookMoves,
-        'queen': board.validQueenMoves,
-        'king': board.validKingMoves
-    };
-    for (var index = 0; index < 0x80; index++) {
-        if (board.onBoard(index)) {
-            for (var piece in piece_function) {
-                board.validMovesTable[piece][index] =
-                    piece_function[piece](index);
-            }
+    } else {
+        diagonals = [-15, -17];
+        if (board.position.pieces[start - rank_delta] == 0) {
+            step = [start - rank_delta];
+        }
+        if (board.getRank(start) == 6) {
+            step = step.concat([start - (rank_delta * 2)]);
         }
     }
+    for (var delta in diagonals) {
+        if (board.position.pieces[start + diagonals[delta]] != 0) {
+            moves.push(start + diagonals[delta]);
+        }
+    }
+    moves = moves.concat(step);
+    board.validMovesTable['pawn'][start] = moves;
 };
 
 /**
  * Checks if a move is valid.
  *
- * Constraints:
+ * Rules & Constraints:
  *
  * 1) Cannot move when it's not your turn.
  * 2) Cannot capture same color pieces.
